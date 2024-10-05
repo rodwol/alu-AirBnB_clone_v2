07423524867d16c8505aea0f2b96bc35592eb358
rodwol <r.goniche@alustudent.com>
2024-10-05 01:52:28 -0700

    commit

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,7 +4,26 @@ from datetime import datetime
 import os
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
+    """
+    Generates a .tgz archive from the contents of the 'web_static' folder.
+
+    This function performs the following steps:
+    1. Defines the source directory as 'web_static' and the
+    destination directory as 'versions'.
+    2. Checks if the 'versions' directory exists, and if not, it creates the directory.
+    3. Creates a timestamp in the format 'YYYYMMDDHHMMSS',
+    which is used in the archive name.
+    4. Builds the archive name in the format 'web_static_<timestamp>.tgz'
+    and constructs the full path where the archive will be saved.
+    5. Uses the `local` function from Fabric to create a tarball (`.tgz`)
+    of the 'web_static' folder inside the 'versions' directory.
+    6. If successful, returns the full path of the generated archive. If any
+    error occurs during the process, it prints the error and returns `None`.
+
+    Returns:
+        str: The full path to the generated archive if successful.
+        None: If an error occurs during the archive creation. 
+    """
     
     # Define the source and version directory
     source_folder = 'web_static'
